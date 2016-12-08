ac52f3f8ed9f1c92932a2ecffaa712c97c6885d3
Emil Tullstedt <sakjur@gmail.com>
2016-12-08 14:32:07 +0100

    F1rst Java EE thingy

---
+++ wildfly/currency/src/is/mjuk/currcon/Convert.java
@@ -0,0 +1,26 @@
+package is.mjuk.currcon;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Convert
+ */
+@WebServlet("/convert")
+public class Convert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Hello, weldt");
+	}
+
+}
